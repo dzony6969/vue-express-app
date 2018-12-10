b61eef4f030bef85d98422c31fdca3a4e6629ba0
dzony6969 <dzony6969@users.noreply.github.com>
2018-12-10 16:39:28 +0100

    connection front with back

--- server/index.js
+++ server/index.js
@@ -16,6 +16,13 @@ app.use(cors());
 app.use('/api/posts', posts);
 app.use('/api/order', order);
 //mongoconnect
+if (process.env.NODE_ENV === 'production') {
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+  
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  }
 
 //locals
   const port = process.env.PORT || 5000;
